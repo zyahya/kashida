26ca69b82d5d5eb95667dd9d24dba5676ac5c35a
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-05-31 20:26:11 +0300

    Refactor Kashida test cases to use constants for input and expected values

--- tests/Kashida.Tests/KashidaApplyTests.cs
+++ tests/Kashida.Tests/KashidaApplyTests.cs
@@ -2,21 +2,24 @@ namespace Kashida.Tests;
 
 public class KashidaApplyTests
 {
+    private readonly static string _normalText = "السلام عليكم";
+    private readonly static string _normalTextExpected = "الـسـلام عـلـيـكـم";
+    private readonly static string _vowelledText = "السَّلَامُ عَلَيْكُمْ";
+    private readonly static string _vowelledTextExpected = "الـسَّـلَامُ عَـلَـيْـكُـمْ";
+
     [Fact]
     public void ApplyKashida_Transforms_Normal_Text()
     {
-        var result = Kashida.ApplyKashida("السلام عليكم");
-        var expectedResult = "الـسـلام عـلـيـكـم";
+        var result = Kashida.ApplyKashida(_normalText);
 
-        Assert.Equal(expectedResult, result);
+        Assert.Equal(_normalTextExpected, result);
     }
 
     [Fact]
     public void ApplyKashida_Transforms_Vowelled_Text()
     {
-        var result = Kashida.ApplyKashida("السَّلَامُ عَلَيْكُمْ");
-        var expectedResult = "الـسَّـلَامُ عَـلَـيْـكُـمْ";
+        var result = Kashida.ApplyKashida(_vowelledText);
 
-        Assert.Equal(expectedResult, result);
+        Assert.Equal(_vowelledTextExpected, result);
     }
 }
--- tests/Kashida.Tests/KashidaClearTests.cs
+++ tests/Kashida.Tests/KashidaClearTests.cs
@@ -2,21 +2,24 @@ namespace Kashida.Tests;
 
 public class KashidaClearTests
 {
+    private readonly static string _normalText = "الـسـلام عـلـيـكـم";
+    private readonly static string _normalTextExpected = "السلام عليكم";
+    private readonly static string _vowelledText = "الـسَّـلَامُ عَـلَـيْـكُـمْ";
+    private readonly static string _vowelledTextExpected = "السَّلَامُ عَلَيْكُمْ";
+
     [Fact]
     public void ClearKashida_GivenNormalText_ShouldReturnClearedText()
     {
-        var result = Kashida.ClearKashida("الـسـلام عـلـيـكـم");
-        var expectedResult = "السلام عليكم";
+        var result = Kashida.ClearKashida(_normalText);
 
-        Assert.Equal(expectedResult, result);
+        Assert.Equal(_normalTextExpected, result);
     }
 
     [Fact]
     public void ClearKashida_GivenVowelledText_ShouldReturnClearedText()
     {
-        var result = Kashida.ClearKashida("الـسَّـلَامُ عَـلَـيْـكُـمْ");
-        var expectedResult = "السَّلَامُ عَلَيْكُمْ";
+        var result = Kashida.ClearKashida(_vowelledText);
 
-        Assert.Equal(expectedResult, result);
+        Assert.Equal(_vowelledTextExpected, result);
     }
 }
