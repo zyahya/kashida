8788e17241e7df053db7332ef4d5dfa554f41595
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-05-31 15:13:52 +0300

    Add tests for both clear and appy functionalities

---
+++ tests/Kashida.Tests/KashidaClearTests.cs
@@ -0,0 +1,22 @@
+namespace Kashida.Tests;
+
+public class KashidaClearTests
+{
+    [Fact]
+    public void ClearKashida_GivenNormalText_ShouldReturnClearedText()
+    {
+        var result = Kashida.Clear("الـسـلام عـلـيـكـم");
+        var expectedResult = "السلام عليكم";
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void ClearKashida_GivenVowelledText_ShouldReturnClearedText()
+    {
+        var result = Kashida.Clear("الـسَّـلَامُ عَـلَـيْـكُـمْ");
+        var expectedResult = "السَّلَامُ عَلَيْكُمْ";
+
+        Assert.Equal(expectedResult, result);
+    }
+}
