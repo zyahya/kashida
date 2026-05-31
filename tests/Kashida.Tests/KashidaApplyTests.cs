b2a7ef8b18e2fa8c813f4ea49f74f08da332c7b7
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-05-31 19:59:10 +0300

    Refactor test cases to use ApplyKashida and ClearKashida methods for consistency

--- tests/Kashida.Tests/KashidaApplyTests.cs
+++ tests/Kashida.Tests/KashidaApplyTests.cs
@@ -5,7 +5,7 @@ public class KashidaApplyTests
     [Fact]
     public void ApplyKashida_Transforms_Normal_Text()
     {
-        var result = Kashida.Apply("السلام عليكم");
+        var result = Kashida.ApplyKashida("السلام عليكم");
         var expectedResult = "الـسـلام عـلـيـكـم";
 
         Assert.Equal(expectedResult, result);
@@ -14,7 +14,7 @@ public class KashidaApplyTests
     [Fact]
     public void ApplyKashida_Transforms_Vowelled_Text()
     {
-        var result = Kashida.Apply("السَّلَامُ عَلَيْكُمْ");
+        var result = Kashida.ApplyKashida("السَّلَامُ عَلَيْكُمْ");
         var expectedResult = "الـسَّـلَامُ عَـلَـيْـكُـمْ";
 
         Assert.Equal(expectedResult, result);
--- tests/Kashida.Tests/KashidaClearTests.cs
+++ tests/Kashida.Tests/KashidaClearTests.cs
@@ -5,7 +5,7 @@ public class KashidaClearTests
     [Fact]
     public void ClearKashida_GivenNormalText_ShouldReturnClearedText()
     {
-        var result = Kashida.Clear("الـسـلام عـلـيـكـم");
+        var result = Kashida.ClearKashida("الـسـلام عـلـيـكـم");
         var expectedResult = "السلام عليكم";
 
         Assert.Equal(expectedResult, result);
@@ -14,7 +14,7 @@ public class KashidaClearTests
     [Fact]
     public void ClearKashida_GivenVowelledText_ShouldReturnClearedText()
     {
-        var result = Kashida.Clear("الـسَّـلَامُ عَـلَـيْـكُـمْ");
+        var result = Kashida.ClearKashida("الـسَّـلَامُ عَـلَـيْـكُـمْ");
         var expectedResult = "السَّلَامُ عَلَيْكُمْ";
 
         Assert.Equal(expectedResult, result);
